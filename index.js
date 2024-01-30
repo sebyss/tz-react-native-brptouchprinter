aa551c9e02aff17689e5865b383362b9a0d4adf4
Miucin Sebastian <sebastian.miucin@tazz.ro>
2024-01-30 14:43:35 +0200

    updated react bridge

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from "react-native";
 
 const { RNBrptouchprinter } = NativeModules;
 
 const component = NativeModules.RNBrptouchprinter;
 
 class BRPtouchPrinter {
-    async print(ipAddress, name, position, company) {
-      return await component.print(ipAddress, name, position, company);
-    }
+	async print(macAddress, pdfPath, labelType) {
+		return await component.print(macAddress, pdfPath, labelType);
+	}
 
-    async getConnectedPrinters() {
-      return await component.getConnectedPrinters();
-    }
+	async getConnectedPrinters() {
+		return await component.getConnectedPrinters();
+	}
 }
 
 export default BRPtouchPrinter;
